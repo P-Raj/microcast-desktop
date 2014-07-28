d76c111eb8f0374a2024008ddda820e5165456be
Pranav Raj <pranav09032@hotmail.com>
2014-07-29 03:38:00 +0530

    Revert "-"
    This reverts commit e340f771679d8cea7fc0220ee3960d04c1cefbae.

---
+++ MicroDownload.py
@@ -0,0 +1,30 @@
+import Peers
+import SegmentHandler as Segments
+
+MAX_BACKLOG = 5
+
+def microDownload(numProcs):
+
+	Segments.downloadMetadata()
+	Peers.initPeers(numProcs)
+
+	while not Segments.allAssigned():
+
+	    peerId = Peers.leastBusyPeer()
+	    peerBackLog = Peers.getBackLog(peerId)
+
+	    if peerBackLog < MAX_BACKLOG:
+	        requestSegment = Segments.getNextUnassigned()
+	        Connection.sendDownloadRequest(peerId, requestSegment)
+	        Segments.assignSegment(feedback["segment"]["id"])
+	        Peers.addBackLog(peerId, requestSegment)
+
+	    else:
+	        feedback = Connection.waitForFeedback()
+
+	    Peers.removeBackLog(peerId, feedback["segment"])
+
+	    if feedback["Status"] == "Failure":
+	        requestSegment = Segments.getNextUnassigned()
+	        Connection.sendDownloadRequest(feedback["from"], requestSegment)
+	        Segments.assignSegment(feedback["segment"]["id"])
