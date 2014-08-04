2cb9b6c99f6d9e8c44e5c75133cbb45131f1b821
P-Raj <pranav09032@hotmail.com>
2014-08-04 15:50:07 +0530

    + TColumn class

--- Terminal.py
+++ Terminal.py
@@ -1,56 +1,58 @@
 import History
 
+
+class TColumn:
+
+	tabSize = 10
+
+	def __init__(self, colNum, totalSegs):
+		self.segDownloaded = []
+		self.toalSegs = totalSegs
+		self.activities = []
+		self.activityCounter = 0
+		self.colNum = colNum
+		self.printSegs = True
+
+	def reachedEnd(self):
+		return self.activityCounter == len(self.activities)
+
+	def getTabSize(self):
+		return " " * tabSize * self.colNum
+
+	def update(self):
+		self.activities = History.getHistory(self.colNum)
+
+	def __gt__(self,other):
+		if not isinstance(other,TColumn):
+			raise Exception("Incomparable")
+			sys.exit(1)
+		if self.reachedEnd():
+			return True
+		return self.getTopTimestamp() > other.getTopTimestamp()
+
+	def __lt__(self,other):
+		if not instance(other,TColumn):
+			raise Exception("Incomparable")
+		if self.reachedEnd():
+			return False
+		return self.getTopTimestamp() < other.getTopTimestamp()
+
+	def getTopTimestamp(self):
+		return self.activities[self.activityCounter][1]
+
+	def addSegment(self, segId):
+		self.segDownloaded.append(segId)
+
+	def __str__(self):
+		if self.printSegs:
+			return self.getTabSize() + str(self.segDownloaded)
+			self.printSegs = False
+		else:
+			self.activityCounter += 1
+			return self.getTabSize()  + self.activities[self.activityCounter-1]
+
+
 class Terminal :
-
-	class TColumn:
-
-		tabSize = 10
-
-		def __init__(self, colNum, totalSegs):
-			self.segDownloaded = []
-			self.toalSegs = totalSegs
-			self.activities = []
-			self.activityCounter = 0
-			self.colNum = colNum
-			self.printSegs = True
-
-		def reachedEnd(self):
-			return self.activityCounter == len(self.activities)
-
-		def getTabSize(self):
-			return " " * tabSize * self.colNum
-
-		def update(self):
-			self.activities = History.getHistory(self.colNum)
-
-		def __gt__(self,other):
-			if not isinstance(other,TColumn):
-				raise Exception("Incomparable")
-				sys.exit(1)
-			if self.reachedEnd():
-				return True
-			return self.getTopTimestamp() > other.getTopTimestamp()
-
-		def __lt__(self,other):
-			if not instance(other,TColumn):
-				raise Exception("Incomparable")
-			if self.reachedEnd():
-				return False
-			return self.getTopTimestamp() < other.getTopTimestamp()
-
-		def getTopTimestamp(self):
-			return self.activities[self.activityCounter][1]
-
-		def addSegment(self, segId):
-			self.segDownloaded.append(segId)
-
-		def __str__(self):
-			if self.printSegs:
-				return self.getTabSize() + str(self.segDownloaded)
-				self.printSegs = False
-			else:
-				self.activityCounter += 1
-				return self.getTabSize()  + self.activities[self.activityCounter-1]
 
 	def __init__(self, numProcs, numSegs):
 		self.numProcs = numProcs
