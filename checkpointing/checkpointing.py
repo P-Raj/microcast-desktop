81b514733a35a8f2aef3d981396465016395cf3b
P-Raj <pranav09032@hotmail.com>
2014-08-08 10:51:19 +0530

    Checkpointing.py : functions added

--- checkpointing/checkpointing.py
+++ checkpointing/checkpointing.py
@@ -1,3 +1,18 @@
+import Queue
+
+class CpMessage:
+
+	def __init__(self, message):
+		self.message = message
+		self.blockBit = False
+		self.senderDependency = []
+
+	def setBlockBit(self):
+		self.blockBit = True
+
+	def setDependency(self, senderDependency):
+		self.senderDependency = senderDependency
+
 
 class CpHandler:
 
@@ -7,6 +22,17 @@ class CpHandler:
 		self.cpEnabled = cpEnabled
 		self.cpTaken = False
 		self.cpInitiator = cpInitiator
+		self.dependency = []
+		self.BQ = self.initBQ()
+
+	def addDependency(self, procId):
+		if procId not in self.dependency:
+			self.dependency.append(procId)
+
+	def initBQ(self):
+		self.BQ = []
+		for _ in range(self.numProcs):
+			self.BQ.append(Queue.Queue())
 
 	def initiateCheckpoint(self):
 		pass
@@ -14,11 +40,24 @@ class CpHandler:
 	def takeCheckpoint(self):
 		pass
 
-	def handleIncomingMsg(self, msg):
-		pass
+	def augmentOutgoingMsg(self, msg):
+		if not self.cpEnabled:
+			return msg
+		else:
+			augMsg = CpMessage(msg)
+			if self.cpTaken:
+				augMsg.setBlockBit()
+			return augMsg
 
-	def handleOutgoingMsg(self, msg):
-		pass
+	def _pushToBQ(self, fromProc, msg):
+		self.BQ[fromProc].put(msg)
+
+	def handleIncomingMsg(self, msg):
+		if type(msg)==type(CpMessage):
+			assert(self.cpEnabled)
+		else:
+			assert(not self.cpEnabled)
+		self.addDependency(msg.message.sender)
 
 	def _getDependentProcs(self):
 		pass
