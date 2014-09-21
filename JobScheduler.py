7d940753b9cd93403c07d841ae3148d5cf8a1b20
P-Raj <pranav09032@hotmail.com>
2014-09-21 12:13:27 +0530

    [FEATURE] Multithreaded microDownload

--- JobScheduler.py
+++ JobScheduler.py
@@ -14,36 +14,27 @@ from datetime import datetime, timedelta
 import threading
 import urllib
 
+
 class JobScheduler:
 
     def __init__(self, environment):
 
         self.MAX_BACKLOG = 5
+
         self.environment = environment
+
         self.SegmentAssignProcId = self.environment.totalProcs - 1
+
         self.segmentHandler = SegmentHandler()
         self.peers = Peers(self.environment.totalProcs)
         self.dataHandler = Datastore.Datastore()
-        self.memoryFile = "memory" + str(self.environment.getMyId()) + ".dump"
-        open(self.memoryFile, "w").close()
 
         self.isDownloading = False
         self.downloadingSegment = None
         self.dwnldStartTime = None
-        self.dwnldLag = 0
 
         self.video_url = "http://127.1.1:8888/"
         self.video_name = "music.mp4"
-
-
-    def dumpMemory(self):
-        out = subprocess.Popen(['ps', 'v', '-p', str(os.getpid())],
-                               stdout=subprocess.PIPE
-                               ).communicate()[0].split(b'\n')
-        vsz_index = out[0].split().index(b'RSS')
-        mem = float(out[1].split()[vsz_index]) / 1024
-        with open(self.memoryFile, "a") as fp:
-            fp.write(str(mem) + "\n")
 
     def isSegmentAssigner(self):
 
@@ -55,108 +46,103 @@ class JobScheduler:
         self.requestQueue = Queue.Queue()
         self.downloadRequests = Queue.Queue()
 
+    """ <Functions to handle local queues> """
+
     def handleAdvertisementQueue(self):
 
-        if not self.toBeAdvertised.empty():
+        while True:
 
-            adMessage = self.toBeAdvertised.get()
-            segmentId = adMessage.messageId
-            Logging.logProcessOp(processId=self.environment.procId,
-                                 op="pop",
-                                 depQueue="AdQ",
-                                 message=adMessage)
+            if not self.toBeAdvertised.empty():
 
-            # broadcast
-            # should change to bcast
-            for _ in range(self.environment.totalProcs-1):
-                if _ != self.environment.getMyId():
-                    reqMessage = adMessage.getResponse()
-                    reqMessage.receiver = _
-                    self.environment.send(_, reqMessage)
-                    Logging.logChannelOp(self.environment.procId,
-                                         _,
-                                         "bcast",
-                                         reqMessage)
+                adMessage = self.toBeAdvertised.get()
+                segmentId = adMessage.messageId
+                Logging.logProcessOp(processId=self.environment.procId,
+                                     op="pop",
+                                     depQueue="AdQ",
+                                     message=adMessage)
 
-    def download(self, segment):
-        self.isDownloading = True
-        self.dwnldStartTime = datetime.now()
-        
-        Logging.logProcessOp(processId=self.environment.procId,
-                             op="startDownloading",
-                             depQueue="",
-                             message=segment)
+                # broadcast
+                # should change to bcast
+                for _ in range(self.environment.totalProcs-1):
+                    if _ != self.environment.getMyId():
+                        reqMessage = adMessage.getResponse()
+                        reqMessage.receiver = _
+                        self.environment.send(_, reqMessage)
+                        Logging.logChannelOp(self.environment.procId,
+                                             _,
+                                             "bcast",
+                                             reqMessage)
 
-
-        dataSeg = urllib.urlopen(self.video_url+"request=True&file=music.mp4&start=0").read()
-        #datsSeg in memory
-
-
-        Logging.logProcessOp(processId=self.environment.procId,
-                             op="downloadComplete",
-                             depQueue="",
-                             message=segment)
-
-        self.dataHandler.addSegment(segment.messageId,
-                                    dataSeg)
-
-        #self.dataHandler.store(self.downloadingSegment)
-        
-        self.isDownloading = False
-        self.dwnldStartTime = None
-        
-        #push it to the ad Queue
-        Logging.logProcessOp(processId=self.environment.procId,
-                             op="push",
-                             depQueue="AdQ",
-                             message=segment)
-
-        self.toBeAdvertised.put(segment)
-        #send response to the initiator
-
-        _response = Message.RequestResponseMessage(
-            senderId=self.environment.procId,
-            messageId=segment.messageId,
-            receiverId=self.SegmentAssignProcId)
-
-        self.environment.send(self.SegmentAssignProcId, _response)
-
-        Logging.logChannelOp(self.environment.procId,
-                             self.SegmentAssignProcId,
-                             "sendConfirmation",
-                             _response)
-
-
-    def downloadingOver(self):
-        return not self.isDownloading
-
-        
     def handleDownloadRequestQueue(self):
 
-	while True:
-		if not self.downloadRequests.empty() and not self.isDownloading:
-        		segment = self.downloadRequests.get()
+        while True:
 
-		        Logging.logProcessOp(processId=self.environment.procId,
-                		             op="pop",
-                            		     depQueue="DwnReqQ",
-                        		     message=segment)
+            if not self.downloadRequests.empty() and not self.isDownloading:
 
-        		downloadThread = threading.Thread(target=self.download, args =[segment])
-        		downloadThread.start()
+                segment = self.downloadRequests.get()
 
+                Logging.logProcessOp(processId=self.environment.procId,
+                                     op="pop",
+                                     depQueue="DwnReqQ",
+                                     message=segment)
 
-        
+                downloadThread = threading.Thread(target=self.download,
+                                                  args=[segment])
+                self.isDownloading = True
+                downloadThread.start()
+
     def handleRequestQueue(self):
-	while True:
-        	if not self.requestQueue.empty():
-            		reqMessage = self.requestQueue.get()
-            		responseMsg = reqMessage.getResponse()
-            		Logging.logProcessOp(processId=self.environment.procId,
-                        		     op="pop",
-                        		     depQueue="ReqQ",
-                                 	     message=reqMessage)
-            		self.environment.send(responseMsg.receiver, responseMsg)
+
+        while True:
+
+            if not self.requestQueue.empty():
+
+                    reqMessage = self.requestQueue.get()
+                    responseMsg = reqMessage.getResponse()
+                    Logging.logProcessOp(processId=self.environment.procId,
+                                         op="pop",
+                                         depQueue="ReqQ",
+                                         message=reqMessage)
+                    self.environment.send(responseMsg.receiver, responseMsg)
+
+    """ </Functions to handle local queues> """
+
+    """ Function to handle incoming channel"""
+
+    def handleIncomingChannelMNc(self):
+
+        while True:
+
+            _message = self.environment._receive()
+
+            if _message:
+
+                Logging.logChannelOp(_message.sender,
+                                     _message.receiver,
+                                     'receive',
+                                     _message)
+
+                if isinstance(_message, Message.DownloadRequestMessage):
+                    assert(_message.sender == self.SegmentAssignProcId)
+                    self.handleDownloadRequestMessage(_message)
+
+                elif isinstance(_message, Message.AdvertisementMessage):
+                    self.handleAdvertisementMessage(_message)
+
+                elif isinstance(_message, Message.RequestMessage):
+                    self.handleRequestMessage(_message)
+
+                elif isinstance(_message, Message.SegmentMessage):
+                    self.handleSegmentMessage(_message)
+
+                elif isinstance(_message, Message.TerminateMessage):
+                    break
+
+                else:
+                    raise Exception("Undefined message \
+                                    found in the channel")
+
+    """ <Functions to handle incoming microDownload Messages> """
 
     def handleDownloadRequestMessage(self, _message):
 
@@ -191,17 +177,31 @@ class JobScheduler:
                                     _message.content)
         self.dataHandler.store(_message)
 
+    """ </Functions to handle incoming microNC Messages> """
+
     def runMicroNC(self):
         self.microNC()
 
-    def stopMicroNC(self):
+    def isMicroNCComplete(self):
 
-        if self.toBeAdvertised.empty() and self.requestQueue.empty() and \
-           self.downloadRequests.empty() and \
-           self.dataHandler.downlodedAll():
-            return True
+        return (self.toBeAdvertised.empty() and self.requestQueue.empty() and
+                self.downloadRequests.empty() and
+                self.dataHandler.downlodedAll())
 
-        return False
+    def detectMicroNCCompetion(self):
+
+        while True:
+            if float(str(self.dataHandler)) == 100:
+                # all segments have been downloaded
+                completionMsg = Message.DownloadCompleteMessage(
+                    senderId=self.environment.procId,
+                    receiverId=self.SegmentAssignProcId)
+                self.environment.send(self.SegmentAssignProcId,
+                                      completionMsg)
+
+            if self.isMicroNCComplete():
+                time.sleep(20)
+                return
 
     def microNC(self):
 
@@ -210,75 +210,91 @@ class JobScheduler:
 
         self.initLocalQueue()
 
-        while True:
+        readingReqQ = self.handleRequestQueue
+        readingDnldReqQ = self.handleDownloadRequestQueue
+        readingAdQ = self.handleAdvertisementQueue
 
+        chanThread = threading.Thread(target=self.handleIncomingChannelMNc)
+        chanThread.start()
 
-            if float(str(self.dataHandler)) == 100:
-                completionMsg = Message.DownloadCompleteMessage(
-                    senderId=self.environment.procId,
-                    receiverId=self.SegmentAssignProcId)
-                self.environment.send(self.SegmentAssignProcId,
-                    completionMsg)
+        reqThread = threading.Thread(target=readingReqQ)
+        reqThread.start()
 
+        dnldThread = threading.Thread(target=readingDnldReqQ)
+        dnldThread.start()
 
-            nonDetchoice = random.randrange(4)
+        adThread = threading.Thread(target=readingAdQ)
+        adThread.start()
 
-            def readingChannels():
+        chanThread.join()
 
-               
-		while True:
-		    _message = self.environment._receive()
-                    if  _message:
-                        Logging.logChannelOp(_message.sender,
-                                             _message.receiver,
-                                             'receive',
-                                             _message)
+        termThread = threading.Thread(tarket=self.detectMicroNCCompetion)
+        termThread.start()
+        termThread.join()
 
-                        if isinstance(_message, Message.DownloadRequestMessage):
-                            assert(_message.sender == self.SegmentAssignProcId)
-                            self.handleDownloadRequestMessage(_message)
-
-                        elif isinstance(_message, Message.AdvertisementMessage):
-                            self.handleAdvertisementMessage(_message)
-
-                        elif isinstance(_message, Message.RequestMessage):
-                            self.handleRequestMessage(_message)
-
-                        elif isinstance(_message, Message.SegmentMessage):
-                            self.handleSegmentMessage(_message)
-
-                        elif isinstance(_message, Message.TerminateMessage):
-                            break
-
-                        else:
-                            raise Exception("Undefined message \
-                                            found in the channel")
-
-            readingReqQ = self.handleRequestQueue
-
-            readingDnldReqQ = self.handleDownloadRequestQueue
-
-            readingAdQ = self.handleAdvertisementQueue
-
-            chanThread = threading.Thread(target=readingChannels)
-            chanThread.start()
-
-	    reqThread = threading.Thread(target=readingReqQ)
-	    reqThread.start()
-
-	    dnldThread = threading.Thread(target=readingDnldReqQ)
-	    dnldThread.start()
-
-	    adThread = threading.Thread(target=readingAdQ)
-	    adThread.start()
-
-	    reqThread.join()
-	    dnldThread.join()
-	    adThread.join()
-            chanThread.join()
+        # TODO: make the following threads non-daemonic
+        reqThread.exit()
+        dnldThread.exit()
+        adThread.exit()
 
     def runMicroDownload(self):
         self.microDownload()
+
+    def download(self, segment):
+
+        self.dwnldStartTime = datetime.now()
+
+        Logging.logProcessOp(processId=self.environment.procId,
+                             op="startDownloading",
+                             depQueue="",
+                             message=segment)
+
+        #TODO : Change the following line
+        segFilename = "music.mp4"
+        segStart = 0
+        dataSeg = urllib.urlopen(self.video_url +
+                                 urllib.urlencode(dict(("request", "True"),
+                                                       ("file", segFilename),
+                                                       ("start", segStart)))
+                                 ).read()
+        #datsSeg in memory
+
+        Logging.logProcessOp(processId=self.environment.procId,
+                             op="downloadComplete",
+                             depQueue="",
+                             message=segment)
+
+        self.dataHandler.addSegment(segment.messageId,
+                                    dataSeg)
+
+        #self.dataHandler.store(self.downloadingSegment)
+
+        self.isDownloading = False
+        self.dwnldStartTime = None
+
+        #push it to the ad Queue
+        Logging.logProcessOp(processId=self.environment.procId,
+                             op="push",
+                             depQueue="AdQ",
+                             message=segment)
+
+        self.toBeAdvertised.put(segment)
+        #send response to the initiator
+
+        _response = Message.RequestResponseMessage(
+            senderId=self.environment.procId,
+            messageId=segment.messageId,
+            receiverId=self.SegmentAssignProcId)
+
+        self.environment.send(self.SegmentAssignProcId, _response)
+
+        Logging.logChannelOp(self.environment.procId,
+                             self.SegmentAssignProcId,
+                             "sendConfirmation",
+                             _response)
+
+    def downloadingOver(self):
+        return not self.isDownloading
 
     def handleRequestResponseMessage(self, _message):
 
@@ -333,6 +349,46 @@ class JobScheduler:
                              op="send",
                              message=requestSegment)
 
+    def handleIncomingChannelMD(self):
+
+        while self.segmentHandler.allAssigned():
+
+            _message = self.environment._receive()
+
+            if _message:
+
+                if isinstance(_message, Message.RequestResponseMessage):
+                    Logging.logChannelOp(_message.sender,
+                                         _message.receiver,
+                                         "recieve",
+                                         _message)
+                    self.handleRequestResponseMessage(_message)
+
+                else:
+                    raise Exception("microDownload received \
+                        undefined message" + str(_message))
+
+                if self.segmentHandler.allDownloaded():
+
+                    for _ in range(self.environment.totalProcs-1):
+                        if _ != self.environment.getMyId():
+                            terminationMsg = Message.TerminateMessage(self.environment.procId, _)
+                            self.environment.send(_, terminationMsg)
+                            Logging.logChannelOp(self.environment.procId,
+                                                 _,
+                                                 "terminateSignal",
+                                                 terminationMsg)
+
+    def handleOutgoingChannelMD(self):
+
+        while True:
+
+            peerId = self.peers.leastBusyPeer()
+            peerBackLog = self.peers.getBackLog(peerId)
+
+            if peerBackLog < self.MAX_BACKLOG:
+                self.sendDownloadRequest(peerId)
+
     def microDownload(self):
 
         Logging.logProcessOp(processId=self.environment.procId,
@@ -341,48 +397,15 @@ class JobScheduler:
         assert(self.environment.procId == self.SegmentAssignProcId)
 
         self.segmentHandler.downloadMetadata(self.video_url, self.video_name)
-	print "Metadata download complete"
+        print "Metadata download complete"
+
         self.peers.initPeers(self.environment.totalProcs - 1)
 
-	nonDetChoice = 0
+        tIn = threading.Thread(target=self.handleIncomingChannelMD)
+        tOut = threading.Thread(target=self.handleOutgoingChannelMD)
 
-        while not self.segmentHandler.allAssigned():
-	    nonDetChoice += 1
-            if nonDetChoice %2 ==  0:
+        tIn.start()
+        tOut.start()
 
-                _message = self.environment._receive()
-
-                if _message:
-
-                    
-                    if isinstance(_message, Message.RequestResponseMessage):
-                        Logging.logChannelOp(_message.sender,
-                                             _message.receiver,
-                                             "recieve",
-                                             _message)
-                        self.handleRequestResponseMessage(_message)
-
-
-                    else:
-                        raise Exception("microDownload received \
-                            undefined message" + str(_message))
-
-                    if self.segmentHandler.allDownloaded():
-
-
-                        for _ in range(self.environment.totalProcs-1):
-                            if _ != self.environment.getMyId():
-                                terminationMsg = Message.TerminateMessage(self.environment.procId, _)
-                                self.environment.send(_, terminationMsg)
-                                Logging.logChannelOp(self.environment.procId,
-                                                     _,
-                                                     "terminateSignal",
-                                                     terminationMsg)
-
-            else:
-
-                peerId = self.peers.leastBusyPeer()
-                peerBackLog = self.peers.getBackLog(peerId)
-
-                if peerBackLog < self.MAX_BACKLOG:
-                    self.sendDownloadRequest(peerId)
+        tIn.join()
+        tOut.join()
