0c29416964bf78aa382bc29b55dd30aa122acb02
Pranav Raj <pranav09032@hotmail.com>
2014-09-27 01:21:02 +0530

    Disabling terminal

--- LogStore.py
+++ LogStore.py
@@ -1,11 +1,11 @@
 import LogViewer
 import os
-from Terminal import Monitor
+#from Terminal import Monitor
 
 terminalOpEnabled = True
 fileOpEnabled = True
 fileOpFolder = "output/"
-terminal = Monitor(1)
+#terminal = Monitor(1)
 
 
 def initRunHistory(procId):
@@ -27,7 +27,7 @@ def writeLog(string):
 		op = op + "(" + string.get("queue","None") + "," + str(string.get("message","None")) + ")"
 	else:
 		raise Exception("Uidentified log")
-	terminal.write(op + "\n")
+	#terminal.write(op + "\n")
 
 
 class File:
